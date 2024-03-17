0858bfe747e4092d06b0aa49a769ada367c0f172
kailastr <mailtokailastr@gmail.com>
2024-03-17 23:59:17 +0530

    Updated Landing Page Navbar

--- src/components/Navbar/LandingPageNav.jsx
+++ src/components/Navbar/LandingPageNav.jsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Navigate } from 'react-router-dom';
 
 const LandingPageNav = () => {
+
+    const [isDropDownOpen, setIsDropDownOpen] = useState(false);
+    console.log(isDropDownOpen);
+
     return (
         <>
             <div className='flex justify-between bg-red-500 text-white w-full py-6 px-20 sticky top-0'>
@@ -13,9 +17,18 @@ const LandingPageNav = () => {
                     <a href="/index#about" className='font-semibold hover:text-red-200'>About Us</a>
                     <a href="/index#working" className='font-semibold hover:text-red-200'>How it works</a>
                 </div>
-                <button className='border px-5 py-2 bg-white text-red-600 hover:bg-red-100 rounded-md font-semibold'>
+                <button onClick={() => { setIsDropDownOpen((prev) => !prev) }} className='border px-5 py-2 bg-white text-red-600 hover:bg-red-100 rounded-md font-semibold'>
                     Join us
                 </button>
+                {isDropDownOpen && (
+                    <div className='absolute right-16 top-20'>
+                        <div className=' bg-white text-red-600 flex flex-col px-5 rounded-lg shadow-lg'>
+                            <h3 className='mx-auto py-2 text-lg font-semibold cursor-pointer hover:text-red-400'>Join as Freelancer</h3>
+                            <hr className='mx-auto border-1 border-red-300 w-28' />
+                            <h3 className='mx-auto py-2 text-lg font-semibold cursor-pointer hover:text-red-400'>Join as Client</h3>
+                        </div>
+                    </div>
+                )}
             </div>
         </>
     )
