04b118b3a39708e76964c4e917e57acf4acc6256
kailastr <mailtokailastr@gmail.com>
2024-03-21 23:42:16 +0530

    Connect Wallet section

--- src/pages/client/ClientIndex.jsx
+++ src/pages/client/ClientIndex.jsx
@@ -3,23 +3,50 @@ import React from 'react';
 //component
 import AddNewGigCard from '../../components/Cards/AddNewGigCard';
 
-//layout
+//wallet
+import { connectConfig } from "../../ConnectKit/Web3Provider";
+import { BasicButton } from "../../ConnectKit/BasicButton";
+import { getAccount } from "@wagmi/core";
 
 const ClientIndex = () => {
-    return (
-        <>
-            <div className='w-full flex justify-center'>
-                <div className='w-11/12 my-10'>
-                    <div className='flex justify-center my-5'>
-                        <h1 className='text-3xl font-semibold'>Add New <span className='text-red-500'>Gig</span></h1>
-                    </div>
+    
+    const account = getAccount(connectConfig);
 
-                    <div className='flex justify-center'>
-                        <AddNewGigCard />
+    return (
+        account.isConnected ?
+            (
+                <div className='w-full flex justify-center'>
+                    <div className='w-11/12 my-10'>
+                        <div className='flex justify-center my-5'>
+                            <h1 className='text-3xl font-semibold'>Add New <span className='text-red-500'>Gig</span></h1>
+                        </div>
+
+                        <div className='flex justify-center'>
+                            <AddNewGigCard />
+                        </div>
                     </div>
                 </div>
-            </div>
-        </>
+            )
+            :
+            (
+                <div className="w-full flex justify-center">
+                    <div
+                        className="w-11/12  border-4 border-blue-400 flex items-center justify-center h-96 rounded-md mt-10"
+                        style={{ backgroundImage: 'linear-gradient(to bottom right, #294a6b, #002447)' }}
+                    >
+                        <div className="flex flex-col">
+                            <h3 className="text-xl text-white font-semibold my-8">
+                                Connect Your wallet to Kickstart your Decentralised Freelancing
+                                Journey
+                            </h3>
+                            <div className="mx-auto">
+                                <BasicButton />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )
+
     )
 }
 
--- src/pages/freelancer/FreelancerIndex.jsx
+++ src/pages/freelancer/FreelancerIndex.jsx
@@ -1,91 +1,95 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 //components
 import GigCard from "../../components/Cards/GigCard";
 
+//wallet
 import { connectConfig } from "../../ConnectKit/Web3Provider";
 import { BasicButton } from "../../ConnectKit/BasicButton";
-
 import { getAccount } from "@wagmi/core";
 
 const FreelancerIndex = () => {
-  const [gigDetails, setGigDetails] = useState([
-    {
-      title: "Web 3 Project",
-      describtion:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Et maiores dolore a magnam incidunt accusantium, beatae suscipit cumque atque dignissimos saepe deleniti consequuntur non doloremque ipsa fugiat repellendus sequi nam?",
-      location: "Kochi",
-      posted: "2024-03-27",
-      price: 999,
-      deadline: "2024-03-27",
-      prerequisite: ["HTML", "JavaScript", "MongoDB", "Blockchain", "C#"],
-    },
-    {
-      title: "Web 3 Project",
-      describtion: "A Freelance working platform",
-      location: "Kochi",
-      posted: "2024-03-27",
-      price: 999,
-      deadline: "2024-03-27",
-      prerequisite: ["React", "Angular"],
-    },
-    {
-      title: "Web 3 Project",
-      describtion: "A Freelance working platform",
-      location: "Kochi",
-      posted: "2024-03-27",
-      price: 999,
-      deadline: "2024-03-27",
-      prerequisite: ["Nodejs", "Express"],
-    },
-    {
-      title: "Web 3 Project",
-      describtion: "A Freelance working platform",
-      location: "Kochi",
-      posted: "2024-03-27",
-      price: 999,
-      deadline: "2024-03-27",
-      prerequisite: ["React", "Shopify"],
-    },
-  ]);
-  const account = getAccount(connectConfig);
-  if (account.isConnected !== true) {
-    return (
-      <>
-        <div className="w-full flex justify-center">
-          <div className="w-11/12 mt-10">
-            <h3 className="my-3 text-xl font-semibold">
-              Connect Your wallet to Kickstart your Decentralised Freelancing
-              Journey
-            </h3>
-            <BasicButton />
-          </div>
-        </div>
-      </>
-    );
-  } else {
-    return (
-      <>
-        <div className="w-full flex justify-center">
-          <div className="w-11/12 mt-10">
-            <div className="flex justify-center mt-5">
-              <h1 className="font-semibold text-3xl">
-                <span className="text-red-500">Gigs</span> You may Like
-              </h1>
-            </div>
+    const [gigDetails, setGigDetails] = useState([
+        {
+            title: "Web 3 Project",
+            describtion:
+                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Et maiores dolore a magnam incidunt accusantium, beatae suscipit cumque atque dignissimos saepe deleniti consequuntur non doloremque ipsa fugiat repellendus sequi nam?",
+            location: "Kochi",
+            posted: "2024-03-27",
+            price: 999,
+            deadline: "2024-03-27",
+            prerequisite: ["HTML", "JavaScript", "MongoDB", "Blockchain", "C#"],
+        },
+        {
+            title: "Web 3 Project",
+            describtion: "A Freelance working platform",
+            location: "Kochi",
+            posted: "2024-03-27",
+            price: 999,
+            deadline: "2024-03-27",
+            prerequisite: ["React", "Angular"],
+        },
+        {
+            title: "Web 3 Project",
+            describtion: "A Freelance working platform",
+            location: "Kochi",
+            posted: "2024-03-27",
+            price: 999,
+            deadline: "2024-03-27",
+            prerequisite: ["Nodejs", "Express"],
+        },
+        {
+            title: "Web 3 Project",
+            describtion: "A Freelance working platform",
+            location: "Kochi",
+            posted: "2024-03-27",
+            price: 999,
+            deadline: "2024-03-27",
+            prerequisite: ["React", "Shopify"],
+        },
+    ]);
 
-            <div className="w-full flex items-center">
-              <div className="my-10 mx-10 w-full">
-                {gigDetails.map((item, index) => (
-                  <GigCard {...item} key={index} />
-                ))}
-              </div>
+    const account = getAccount(connectConfig);
+
+    return (
+        account.isConnected ?
+            (
+                <div className="w-full flex justify-center">
+                    <div className="w-11/12 mt-10">
+                        <div className="flex justify-center mt-5">
+                            <h1 className="font-semibold text-3xl">
+                                <span className="text-red-500">Gigs</span> You may Like
+                            </h1>
+                        </div>
+
+                        <div className="w-full flex items-center">
+                            <div className="my-10 mx-10 w-full">
+                                {gigDetails.map((item, index) => (
+                                    <GigCard {...item} key={index} />
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )
+            :
+            <div className="w-full flex justify-center">
+                <div
+                    className="w-11/12  border-4 border-blue-400 flex items-center justify-center h-96 rounded-md mt-10"
+                    style={{ backgroundImage: 'linear-gradient(to bottom right, #294a6b, #002447)' }}
+                >
+                    <div className="flex flex-col">
+                        <h3 className="text-xl text-white font-semibold my-8">
+                            Connect Your wallet to Kickstart your Decentralised Freelancing
+                            Journey
+                        </h3>
+                        <div className="mx-auto">
+                            <BasicButton />
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-      </>
     );
-  }
 };
 
 export default FreelancerIndex;
