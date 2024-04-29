6858b044f7f016e1ae9afb75771507a86ecf877f
Jaseempk <jasimpk011@gmail.com>
2024-04-29 11:31:03 +0530

    feat:updated client profile component

--- src/pages/client/ClientProfile.jsx
+++ src/pages/client/ClientProfile.jsx
@@ -30,7 +30,7 @@ const ClientProfile = () => {
         .single();
 
       if (error) {
-        setFetchError("could not fetch the existing gigs");
+        setFetchError("could not fetch the profile ");
         console.log(error);
       }
       if (data) {
@@ -47,10 +47,14 @@ const ClientProfile = () => {
     FirstName: "Allen",
     MiddleName: "Jack",
     LastName: "Taylor",
-    location: "India-Kerala-Kochi",
     description:
       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore architecto culpa ab, expedita alias dolores tenetur perferendis deleniti voluptas fugiat ut ipsa? Deleniti culpa quibusdam nemo itaque eveniet neque reprehenderit.",
+    badgeCoins: 650,
     languagesKnown: ["English", "Malayalam", "Hindi"],
+    skills: ["Web Development", "PhotoGraphy", "Photoshop"],
+    ExperienceLevel: "Expert",
+    PortfolioLink: "",
+    walletAddress: "369#2255",
   });
 
   const [experienceLevel, setExperienceLevel] = useState(user.ExperienceLevel);
@@ -140,7 +144,7 @@ const ClientProfile = () => {
           </div>
         </div>
         <div className="flex justify-center my-5" id="profileView">
-          <div className="bg-red-100 w-8/12 rounded-md flex flex-row py-10">
+          <div className="bg-red-100 w-8/12 rounded-md flex flex-row">
             <div className="w-4/12 flex flex-col my-auto px-3 border-r-2 border-red-500">
               <img
                 src="https://us.123rf.com/450wm/odkyrylov/odkyrylov2002/odkyrylov200200003/140021006-user-sign-icon-person-symbol-human-avatar-flat-style.jpg"
@@ -149,10 +153,14 @@ const ClientProfile = () => {
               />
               <h3 className="mx-auto font-medium text-md">{`Name : ${user.FirstName} ${user.MiddleName} ${user.LastName}`}</h3>
               <div className="flex justify-center gap-2">
+                <IoIosInformationCircleOutline
+                  className="my-auto text-xl"
+                  title="Coins 500 : Intermediate level, 500+ : Expert, 500- : bad rating"
+                />
                 <LuBadgeCheck className="my-auto bg-blue-400 rounded-full text-xl" />
                 <h3 className="font-normal text-md ">
                   {" "}
-                  Verified User
+                  Badge Coins : {user.badgeCoins}{" "}
                 </h3>
               </div>
             </div>
@@ -170,17 +178,54 @@ const ClientProfile = () => {
               <div className="flex gap-10 my-3 font-semibold">
                 <div className="flex gap-2">
                   <GrLocation className="h-5 w-5 my-auto" />
-                  <p>{user.location}</p>
+                  <p>India-Kerala-Kochi</p>
                 </div>
+                <div className="cursor-pointer hover:text-red-500">
+                  <a
+                    href={`${user.PortfolioLink}`}
+                    target="_blank"
+                    className="flex gap-2 "
+                  >
+                    <IoIosLink className="h-5 w-5 my-auto" />
+                    <p>Portfolio Link</p>
+                  </a>
+                </div>
+                <div className="flex gap-2">
+                  <IoWalletOutline className="h-5 w-5 my-auto" />
+                  <p>{user.walletAddress}</p>
+                </div>
+              </div>
+              <div className="flex gap-10  my-3">
                 <div className="flex gap-3">
                   <MdWorkOutline className="my-auto text-lg" />
                   <h3 className="font-semibold">Works Committed</h3>
                   <p>2</p>
                 </div>
+                <div className="flex gap-3">
+                  <FaRegStar className="my-auto text-lg" />
+                  <h3 className="font-semibold">Rating</h3>
+                  <p>4/5</p>
+                </div>
+              </div>
+              <div className="flex gap-3 my-3">
+                <GrSemantics className="my-auto text-lg" />
+                <h3 className="font-semibold">Experience Level : </h3>
+                <p className="font-semibold">{user.ExperienceLevel}</p>
               </div>
               <div className="flex gap-5 my-3">
                 <h3 className="font-semibold">Languages : </h3>
                 {user.languagesKnown.map((item, index) => (
+                  <p
+                    key={index}
+                    className="text-sm my-auto px-2 rounded-lg bg-red-50"
+                  >
+                    {item}
+                  </p>
+                ))}
+              </div>
+              <div className="flex gap-5 my-3">
+                <h3 className="font-semibold">Skills : </h3>
+                {user.skills.map((item, index) => (
                   <p
                     key={index}
                     className="text-sm my-auto px-2 rounded-lg bg-red-50"
@@ -246,6 +291,51 @@ const ClientProfile = () => {
                 className="flex justify-start mx-5 mt-3 font-medium w-11/12 px-3 rounded-md py-2"
               />
 
+              <label
+                htmlFor="walletAddress"
+                className="flex justify-start mx-5 mt-3 font-medium"
+              >
+                Wallet Address :{" "}
+              </label>
+              <input
+                type="text"
+                id="walletAddress"
+                placeholder={user.walletAddress}
+                className="flex justify-start mx-5 mt-3 font-medium w-11/12 px-3 rounded-md py-2"
+              />
+
+              <div className="mt-3">
+                <label
+                  htmlFor="experienceLevel"
+                  className="flex justify-start mx-5 mt-3 font-medium"
+                >
+                  Experience Level :{" "}
+                </label>
+                <select
+                  id="experienceLevel"
+                  className="flex justify-start mx-5 mt-3 font-medium w-11/12 px-3 rounded-md py-2"
+                  value={experienceLevel}
+                  onChange={handleExperienceLevelChange}
+                >
+                  <option value="Beginner">Beginner</option>
+                  <option value="Intermediate">Intermediate</option>
+                  <option value="Expert">Expert</option>
+                </select>
+              </div>
+
+              <label
+                htmlFor="portfolioLink"
+                className="flex justify-start mx-5 mt-3 font-medium"
+              >
+                Portfolio Link :{" "}
+              </label>
+              <input
+                type="text"
+                id="portfolioLink"
+                placeholder="https://...."
+                className="flex justify-start mx-5 mt-3 font-medium w-11/12 px-3 rounded-md py-2"
+              />
+
               <div className="mt-3">
                 <h3 className="font-medium mx-5">Languages known</h3>
                 <div className="flex gap-2 mx-5 mt-3">
@@ -274,6 +364,40 @@ const ClientProfile = () => {
                       <IoMdClose
                         className="my-auto ml-2 cursor-pointer"
                         onClick={() => removeLanguage(index)}
+                      />
+                    </div>
+                  ))}
+                </div>
+              </div>
+
+              <div className="mt-3">
+                <h3 className="font-medium mx-5">Skills</h3>
+                <div className="flex gap-2 mx-5 mt-3">
+                  <input
+                    type="text"
+                    id="newskill"
+                    placeholder="Add new skill"
+                    className="flex justify-start font-medium w-8/12 px-3 rounded-md py-2"
+                  />
+                  <button
+                    className="mx-5 font-medium bg-slate-300 px-5 rounded-md"
+                    onClick={() =>
+                      addSkill(document.getElementById("newskill").value)
+                    }
+                  >
+                    Add
+                  </button>
+                </div>
+                <div className="flex gap-3 mx-5 mt-3">
+                  {user.skills.map((skill, index) => (
+                    <div
+                      className="bg-red-50 px-2 py-1 text-xs rounded-xl flex my-auto"
+                      key={index}
+                    >
+                      {skill}
+                      <IoMdClose
+                        className="my-auto ml-2 cursor-pointer"
+                        onClick={() => removeSkill(index)}
                       />
                     </div>
                   ))}
